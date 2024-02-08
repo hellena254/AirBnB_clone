3e0f9741915b881a3d645b0fb400b9a459699f99
hellena254 <hellenah243@gmail.com>
2024-02-08 05:30:23 -0800

    implement console 0.0.1

---
+++ console.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+ contains the entry point of the command interpreter
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+	prompt = '(hbnb) '
+
+	def do_quit(self, args):
+		"""Quit command to exit the program
+
+		"""
+		return True
+
+	def do_EOF(self, args):
+		"""EOF command to exit program
+
+		"""
+		print()
+		return True
+	
+	def emptyline(self):
+        	"""Do nothing on empty input line"""
+        	pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
--- models/base_model.py
+++ models/base_model.py
@@ -57,6 +57,13 @@ class BaseModel:
 
 
 # tests
+all_objs = storage.all()
+print("-- Reloaded objects --")
+for obj_id in all_objs.keys():
+    obj = all_objs[obj_id]
+    print(obj)
+
+print("-- Create a new object --")
 my_model = BaseModel()
 my_model.name = "hellen"
 my_model.save()
